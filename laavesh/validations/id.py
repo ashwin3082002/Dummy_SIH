645eec7d60ba78343740b18d452a94eafbcf004a
aviiciii <laavesh1@gmail.com>
2022-08-16 21:55:09 +0530

    updated validations

--- laavesh/validations/id.py
+++ laavesh/validations/id.py
@@ -19,7 +19,7 @@ def parse_id_stud(s):
 
     :param s: ID
     :type s: str
-    :return: A dict of {'year': 0000 and 'serial' : 'xxxxx'}
+    :return: A dict of {'year': 0000 and 'serial' : 'xxxxx0'}
     :rtype: dict
     """
     if search := re.search(r'^(\d{4})([a-z]{5}\d{1})$', s, re.IGNORECASE):
@@ -43,23 +43,6 @@ def check_id_insti(s):
     else:
         return False
 
-def parse_id_insti(s):
-    """
-    Parses the id and returns the year(1234) and serial(abcde) as a dict
-
-    :param s: ID
-    :type s: str
-    :return: A dict of {'year': 0000 and 'serial' : 'xxxxx'}
-    :rtype: dict
-    """
-    if search := re.search(r'^([a-z]{5})$', s, re.IGNORECASE):
-        id_temp = list(search.groups())
-        return {
-            'year':int(id_temp[0]),
-            'serial':id_temp[1].lower()    
-        }
-    else:
-        return False
 
 inpt = input('Stud ID: ')
 
@@ -67,3 +50,5 @@ print('check_id:', check_id_stud(inpt))
 print('parse_id:', parse_id_stud(inpt))
 
 inpt = input('Insti ID: ')
+
+print('check_id:', check_id_insti(inpt))
