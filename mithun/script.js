ba83addc57e8473b6d22bfaba8eb6a7471c650e6
kmithunraj <kmithunraj@mithunraj.com>
2022-08-16 12:34:45 +0530

    Revert "student dashboard"
    This reverts commit 9c16a882b4ab9f5d8d3242a97ce5cd82edb34028.

--- mithun/script.js
+++
@@ -1,8 +0,0 @@
-function navbtn() {
-  var x = document.getElementById("navbar");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
